21204af46968d41d736599962ebccbdaecb38a64
douglasblnk <douglasblank1@gmail.com>
2021-06-12 15:32:15 -0300

    feat

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import WindiCSS from 'vite-plugin-windicss';
+import ViteComponents from 'vite-plugin-components';
+import Pages from 'vite-plugin-pages';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    Pages(),
+    WindiCSS(),
+    VitePWA(),
+    ViteComponents({
+      dirs: [
+        'src/components',
+        'src/pages',
+      ],
+    }),
+  ],
+});
