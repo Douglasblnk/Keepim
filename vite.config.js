fac1d3537cba8219cc6a86adb9475eab7d22d001
douglasblnk <douglasblank1@gmail.com>
2021-06-19 01:23:40 -0300

    feat: added new dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
 import ViteComponents from 'vite-plugin-components';
 import Pages from 'vite-plugin-pages';
+import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Pages(),
     WindiCSS(),
-    VitePWA(),
+    VitePWA({
+      manifest: {
+        name: 'pwa',
+        short_name: 'pwa',
+        icons: [],
+        start_url: '/index.html',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#4DBA87'
+      }
+    }),
     ViteComponents({
       dirs: [
         'src/components',
