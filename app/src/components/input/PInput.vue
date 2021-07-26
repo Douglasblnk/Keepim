3bbec99387ee0516817b302456c1463b6ba5e8ae
Douglas Blank <douglasblank1@gmail.com>
2021-07-25 23:23:12 -0300

    feat: Created new PInput component

---
+++ app/src/components/input/PInput.vue
@@ -0,0 +1,47 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PInput',
+
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <input
+    class="p-input"
+    :type="type"
+    :placeholder="placeholder"
+  >
+</template>
+
+<style lang="postcss">
+@layer components {
+  .p-input {
+    @apply
+      flex
+      w-full
+      border-none
+      rounded-full
+      px-5
+      py-3
+      outline-none
+      !bg-gray-100;
+  }
+}
+</style>
