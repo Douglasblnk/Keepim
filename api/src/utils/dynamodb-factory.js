f2f73198db067c1c476ef7846973188d44ce5473
Douglas Blank <douglasblank1@gmail.com>
2021-08-20 01:35:37 -0300

    feat: Implemented dynamodb factory function

---
+++ api/src/utils/dynamodb-factory.js
@@ -0,0 +1,20 @@
+import AWS from 'aws-sdk'
+
+export default function dynamodbFactory () {
+  function getOptions ({ options, offlinePort } = {}) {
+    if (options) return options;
+
+    else if (process.env.IS_OFFLINE)
+      return { region: "localhost", endpoint: `http://localhost:${offlinePort || 8000}` }
+    else return { region: 'sa-east-1' };
+  }
+  
+  function doc({ options, offlinePort } = {}) {
+    return new AWS.DynamoDB.DocumentClient(getOptions({ options, offlinePort }));
+  }
+
+  return {
+    doc,
+    getOptions,
+  }
+}
