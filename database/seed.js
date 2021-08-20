e0dfdf8b1e7b3413116e083be38169a27f800d6b
Douglas Blank <40043147+Douglasblnk@users.noreply.github.com>
2021-08-20 07:57:53 -0300

    Update seed.js

--- database/seed.js
+++ database/seed.js
@@ -1,4 +1,19 @@
+const AWS = require('aws-sdk');
 const minimist = require('minimist')
 const args = minimist(process.argv.slice(2))
 
-console.log(args);
+(async () => {
+  const { id } = args;
+  await getUserTable(id);
+})()
+
+function checkFilesExistence() {
+
+} 
+
+async function getUserTable(id) {
+  try {
+    
+  } catch (err) {
+ } 
+} 
