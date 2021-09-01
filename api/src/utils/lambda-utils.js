cedea96a79e45305d0fcfe92367cb46037b6a646
Douglas Blank <douglasblank1@gmail.com>
2021-09-01 07:57:46 -0300

    feat: Initial development of the new endpoint

--- api/src/utils/lambda-utils.js
+++ api/src/utils/lambda-utils.js
@@ -2,6 +2,40 @@ export const isString = arg => typeof arg === 'string';
 
 export const objToStr = arg => ((isString(arg)) ? arg : JSON.stringify(arg));
 
+const { isObject } = require('deep-object-js');
+
+const _validateParams = (event) => {
+  if (!isObject(event)) throw new Error('Event is not an object');
+}
+
+/**
+ * Faz o parse do Body de uma requisição se existir no event, caso seja necessário.
+ * 
+ * @requires {object} event Lambda event
+ * 
+ * @example
+ * const myLambda = (event, context, callback) => {
+ *  / event = undefined;
+ *  / let body = getBody(event, {}); // -> {}  
+ *  / body = getBody(event); // -> null
+ * }
+ * 
+ */
+
+const getBody = (event, defaultValue = null) => {
+  try {
+    _validateParams(event);
+    if (typeof event.body === 'string') return JSON.parse(event.body);
+    
+    if (!isObject(event.body)) return defaultValue;
+
+    return event.body;
+  } catch (err) {
+    return defaultValue;
+  }
+}
+
+
 export const lambdaResp = (statusCode, body, headers = {}) => ({ statusCode, ...(body ? { body: objToStr(body) } : ''), headers });
 
 export const lambdaRespErr = ({
