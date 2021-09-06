f5935fbd950c0ed8e017d400f6fb5bcf3f34143e
Douglas Blank <douglasblank1@gmail.com>
2021-09-05 23:00:20 -0300

    feat: eslinted files

--- api/src/routes/user/post.js
+++ api/src/routes/user/post.js
@@ -1,20 +1,23 @@
-import { lambdaResp, lambdaRespErr, getBody } from '@/utils/lambda-utils'
+import { lambdaResp, lambdaRespErr, getBody } from '@/utils/lambda-utils';
 
-function getParameters (event) {
+function getParameters(event) {
   return {
-    ...getBody(event)
-  }
+    ...getBody(event),
+  };
 }
 
-export async function run (event) {
+export async function run(event) {
   try {
-    const parameters = getParameters(event)
+    const parameters = getParameters(event);
+
     console.log('parameters :>> ', parameters);
 
-    return lambdaResp(200, {result: 'success'})
+    return lambdaResp(200, { result: 'success' });
   } catch (error) {
     console.log('err authValidate:>> ', error);
 
-    return lambdaRespErr(error)
+    return lambdaRespErr(error);
   }
 }
+
+export default run;
--- api/src/utils/dynamodb-factory.js
+++ api/src/utils/dynamodb-factory.js
@@ -1,14 +1,13 @@
-import AWS from 'aws-sdk'
+import AWS from 'aws-sdk';
 
-export default function dynamodbFactory () {
-  function getOptions ({ options, offlinePort } = {}) {
+export default function dynamodbFactory() {
+  function getOptions({ options, offlinePort } = {}) {
     if (options) return options;
 
-    else if (process.env.IS_OFFLINE)
-      return { region: "localhost", endpoint: `http://localhost:${offlinePort || 8000}` }
-    else return { region: 'sa-east-1' };
+    if (process.env.IS_OFFLINE) { return { region: 'localhost', endpoint: `http://localhost:${offlinePort || 8000}` }; }
+    return { region: 'sa-east-1' };
   }
-  
+
   function doc({ options, offlinePort } = {}) {
     return new AWS.DynamoDB.DocumentClient(getOptions({ options, offlinePort }));
   }
@@ -16,5 +15,5 @@ export default function dynamodbFactory () {
   return {
     doc,
     getOptions,
-  }
+  };
 }
