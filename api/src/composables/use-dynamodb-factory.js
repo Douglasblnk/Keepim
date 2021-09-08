fc14f215b849b788c4d0e24984ce34c7f68876d9
Douglas Blank <douglasblank1@gmail.com>
2021-09-08 18:54:19 -0300

    feat: changed dynamo factory to composable function

--- api/src/composables/use-dynamodb-factory.js
+++ api/src/composables/use-dynamodb-factory.js
@@ -1,16 +1,14 @@
 import AWS from 'aws-sdk';
 
-export default function dynamodbFactory() {
-  function getOptions({ options, offlinePort } = {}) {
+export default function dynamodbFactory({ options, offlinePort } = {}) {
+  const getOptions = () => {
     if (options) return options;
 
     if (process.env.IS_OFFLINE) { return { region: 'localhost', endpoint: `http://localhost:${offlinePort || 8000}` }; }
     return { region: 'sa-east-1' };
-  }
+  };
 
-  function doc({ options, offlinePort } = {}) {
-    return new AWS.DynamoDB.DocumentClient(getOptions({ options, offlinePort }));
-  }
+  const doc = () => new AWS.DynamoDB.DocumentClient(getOptions({ options, offlinePort }));
 
   return {
     doc,
