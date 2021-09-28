c517782e99509c1eaa2d99eb17ec8c64acf1b034
Douglas Blank <douglasblank1@gmail.com>
2021-09-28 19:17:12 -0300

    feat: initial implementation of user validation

--- api/src/lib/services/auth/validate-user.js
+++ api/src/lib/services/auth/validate-user.js
@@ -1,3 +1,13 @@
-export default async function validateUser({ email, password }) {
-  console.log('email, password :>> ', email, password);
+import userModel from '@/lib/models/user';
+
+async function getUser(user, password) {
+  const { queryUser } = userModel({ user, password });
+
+  return queryUser();
+}
+
+export default async function validateUser({ user, password }) {
+  const userFound = await getUser(user, password);
+
+  console.log('userFound :>> ', userFound);
 }
