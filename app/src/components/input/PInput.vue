9515fa985a80acb34d0aae4fff4d6db1795a32b0
Douglas Blank <douglasblank1@gmail.com>
2021-09-28 19:15:45 -0300

    feat: removed unnecesary implementation and atributified code

--- app/src/components/input/PInput.vue
+++ app/src/components/input/PInput.vue
@@ -1,83 +1,49 @@
-<script>
-import { computed, defineComponent } from 'vue';
+<script setup>
+const emit = defineEmits(['update:modelValue']);
 
-export default defineComponent({
-  name: 'PInput',
-
-  emits: ['update:modelValue'],
-
-  props: {
-    modelValue: {
-      type: [String, Number],
-      default: '',
-    },
-
-    type: {
-      type: String,
-      default: 'text',
-    },
-
-    placeholder: {
-      type: String,
-      default: '',
-    },
-
-    error: {
-      type: String,
-      default: '',
-    },
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    default: '',
   },
 
-  setup(props, { emit }) {
-    const model = computed({
-      get: () => props.modelValue,
-      set: value => emit('update:modelValue', value),
-    });
-
-    return {
-      model,
-    };
+  type: {
+    type: String,
+    default: 'text',
   },
+
+  placeholder: {
+    type: String,
+    default: '',
+  },
+
+  errorMsg: {
+    type: String,
+    default: '',
+  },
+
+  error: Boolean,
+});
+
+const model = computed({
+  get: () => props.modelValue,
+  set: value => emit('update:modelValue', value),
 });
 </script>
 
 <template>
-  <div class="flex flex-col">
+  <div w:flex="~ col">
     <input
       v-model="model"
-      class="p-input"
-      :w:border="error ? '1 solid danger' : ''"
+      w:rounded="full"
+      w:flex="~"
+      w:outline="none"
+      w:bg="gray-100"
+      w:w="full"
+      w:p="y-3 x-5"
+      :w:border="error ? '1 solid danger' : 'none'"
       :type="type"
       :placeholder="placeholder"
     >
-
-    <div
-      w:flex="~"
-      w:justify="center"
-      w:h="min-lg"
-    >
-      <small
-        v-show="error"
-        w:m="x-sm t-sm"
-        w:pos="absolute"
-        w:text="danger"
-      >
-        {{ error }}
-      </small>
-    </div>
   </div>
 </template>
-
-<style lang="postcss">
-.p-input {
-  @apply
-    flex
-    w-full
-    border-none
-    rounded-full
-    px-5
-    py-3
-    outline-none
-    bg-gray-100;
-}
-</style>
