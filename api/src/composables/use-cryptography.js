c6c5b1837da150b0c1f6da8d557188c2c85a70af
Douglas Blank <douglasblank1@gmail.com>
2021-09-29 23:35:20 -0300

    feat: improved use cryptography composable

--- api/src/composables/use-cryptography.js
+++ api/src/composables/use-cryptography.js
@@ -1,12 +1,15 @@
-import { genSalt, hash } from 'bcrypt';
+import { genSalt, hash, compare } from 'bcrypt';
 
 export default function useCryptography() {
-  const genSaltRounds = async () => genSalt(Number(process.env.saltRound));
+  const genSaltRounds = async () => genSalt(Number(process.env.SALT_ROUND));
 
   const hashPassword = async (password, salt) => hash(password, salt);
+
+  const comparePassword = async (hashedPassword, password) => compare(password, hashedPassword);
 
   return {
     genSaltRounds,
     hashPassword,
+    comparePassword,
   };
 }
