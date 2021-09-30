30b29b2ebd20a31927ec0b4ea26f6f422021eb98
Douglas Blank <douglasblank1@gmail.com>
2021-09-29 23:36:03 -0300

    feat: implemented user login validation

--- api/src/lib/services/auth/validate-user.js
+++ api/src/lib/services/auth/validate-user.js
@@ -1,13 +1,43 @@
 import userModel from '@/lib/models/user';
+import useCryptography from '@/composables/use-cryptography';
+import useAuthentication from '@/composables/use-authentication';
+import useError from '@/composables/use-error';
 
-async function getUser(user, password) {
-  const { queryUser } = userModel({ user, password });
+const { notFoundError, unauthorizedError } = useError();
+
+async function getUser(id, password) {
+  const { queryUser } = userModel({ id, password });
 
   return queryUser();
 }
 
-export default async function validateUser({ user, password }) {
-  const userFound = await getUser(user, password);
+async function validatePassword({ password: hash }, password) {
+  const { comparePassword } = useCryptography();
 
-  console.log('userFound :>> ', userFound);
+  const match = await comparePassword(hash, password);
+
+  if (!match) throw unauthorizedError('Incorrect password');
+}
+
+function generateToken(user) {
+  const { signJwt } = useAuthentication();
+
+  const {
+    password,
+    createdAt,
+    updatedAt,
+    ...parsedUser
+  } = user;
+
+  return signJwt(parsedUser);
+}
+
+export default async function validateUser({ id, password }) {
+  const [userFound] = await getUser(id, password);
+
+  if (!userFound) throw notFoundError('User not Found');
+
+  await validatePassword(userFound, password);
+
+  return generateToken(userFound);
 }
