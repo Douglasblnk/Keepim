fe3ac00fd562ad1dadd7c80df74d249da1fa23f1
Douglas Blank <douglasblank1@gmail.com>
2021-10-12 01:57:19 -0300

    feat: added transition to app

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,15 +5,27 @@ const { isShowing, alertText, alertType } = useAlert();
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Suspense>
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <component :is="Component" />
+      </transition>
+    </Suspense>
+  </RouterView>
 
   <PAlert
     :is-showing="isShowing"
     :type="alertType"
     :text="alertText"
   />
+
+  <PBottomNav />
 </template>
 
 <style>
 @import './styles/styles.css';
+@import './styles/transitions.css';
 </style>
