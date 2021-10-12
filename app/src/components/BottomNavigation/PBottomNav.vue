ba61b9c6a14ef5e2aec277b093f7bcdf067dbf09
Douglas Blank <douglasblank1@gmail.com>
2021-10-12 02:00:41 -0300

    feat: initial implementation of bottom navigation

---
+++ app/src/components/BottomNavigation/PBottomNav.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div
+    w:pos="fixed bottom-4"
+    w:w="full"
+    w:flex="~"
+    w:justify="center"
+    w:m="x-auto"
+  >
+    <div
+      w:w="3/5"
+      w:p="x-xl y-md"
+      w:rounded="full"
+      w:bg="gray-100 opacity-5"
+      w:backdrop="~ blur-md"
+      w:text="white"
+    >
+      poxa
+    </div>
+  </div>
+</template>
