35157d09adc37571f64db0669652e7356e74e1d3
Douglas Blank <douglasblank1@gmail.com>
2021-10-14 22:36:35 -0300

    feat: added new icons

--- app/src/utils/icons.js
+++ app/src/utils/icons.js
@@ -3,11 +3,17 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faArrowRight,
   faSpinner,
+  faThLarge,
+  faHome,
+  faThList,
 } from '@fortawesome/free-solid-svg-icons';
 
 export default function() {
   library.add([
     faArrowRight,
     faSpinner,
+    faThLarge,
+    faHome,
+    faThList,
   ]);
 }
