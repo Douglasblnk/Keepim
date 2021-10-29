e3f58b2f38defbe9e3d9c34c3a2af626462ed0a0
Douglas Blank <douglasblank1@gmail.com>
2021-10-29 00:42:42 -0300

    feat: Initial development of img component

---
+++ app/src/components/atoms/Img.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  url: String,
+
+  style: String,
+
+  alt: String,
+});
+</script>
+
+<template>
+  <div>
+    <img
+      w:rounded="md"
+      :src="url"
+      :style="style"
+      :alt="alt"
+    >
+  </div>
+</template>
