a35bc2b5f103b90fd3902d43fde5efdda165c1a4
Douglas Blank <douglasblank1@gmail.com>
2021-10-28 21:18:06 -0300

    feat: Developed shotcut action component

---
+++ app/src/components/organisms/shortcut-action/ShortcutAction.vue
@@ -0,0 +1,80 @@
+<script setup>
+defineEmits([
+  'add-folder',
+  'add-image',
+  'add-category',
+]);
+</script>
+
+<template>
+  <div w:flex="~ col">
+    <p w:text="medium">
+      Atalhos
+    </p>
+
+    <div
+      w:flex="~ wrap"
+      w:m="t-sm"
+    >
+      <div
+        w:flex="~ col"
+        w:align="items-center"
+        w:p="md"
+        w:m="r-sm b-sm"
+        w:bg="ascendent"
+        w:rounded="md"
+        w:cursor="pointer"
+        @click="$emit('add-folder')"
+      >
+        <FIcon
+          icon="folder-plus"
+          w:m="b-sm"
+        />
+
+        <p w:select="none">
+          Adicionar pasta
+        </p>
+      </div>
+
+      <div
+        w:flex="~ col"
+        w:align="items-center"
+        w:p="md"
+        w:m="r-sm b-sm"
+        w:bg="ascendent"
+        w:rounded="md"
+        w:cursor="pointer"
+        @click="$emit('add-image')"
+      >
+        <FIcon
+          icon="images"
+          w:m="b-sm"
+        />
+
+        <p w:select="none">
+          Adicionar imagem
+        </p>
+      </div>
+
+      <div
+        w:flex="~ col"
+        w:align="items-center"
+        w:p="md"
+        w:m="r-sm b-sm"
+        w:bg="ascendent"
+        w:rounded="md"
+        w:cursor="pointer"
+        @click="$emit('add-category')"
+      >
+        <FIcon
+          icon="tags"
+          w:m="b-sm"
+        />
+
+        <p w:select="none">
+          Adicionar categoria
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
