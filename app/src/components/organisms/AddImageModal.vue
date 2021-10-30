2bdc552c01c2b1b83c15ad4760300242a2499bf5
Douglas Blank <douglasblank1@gmail.com>
2021-10-30 03:58:41 -0300

    feat: initial implementation of image modal

---
+++ app/src/components/organisms/AddImageModal.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  folders: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+defineEmits(['close', 'confirm']);
+</script>
+
+<template>
+  Add image modal
+</template>
