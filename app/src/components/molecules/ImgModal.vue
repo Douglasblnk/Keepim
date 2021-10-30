091c747b93a6b8b9263a95a265ad0f877903237f
Douglas Blank <douglasblank1@gmail.com>
2021-10-30 03:58:09 -0300

    feat: initial development of the imgModal

---
+++ app/src/components/molecules/ImgModal.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+  url: {
+    type: String,
+    default: '',
+  },
+
+  categories: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+defineEmits(['close']);
+</script>
+
+<template>
+  <div
+    w:flex="~ col"
+    w:justify="between"
+    w:h="60vh"
+    w:w="90vw"
+  >
+    <div
+      w:flex="~ 1"
+      w:justify="center"
+      w:align="items-center"
+    >
+      <Img
+        :url="url"
+      />
+    </div>
+
+    <div
+      w:flex="~ 2"
+      w:justify="center"
+    >
+      <Button
+        icon="times"
+        icon-size="lg"
+        w:w="!xxl"
+        w:h="!xxl"
+        circle
+        primary
+        @click="$emit('close')"
+      />
+    </div>
+  </div>
+</template>
