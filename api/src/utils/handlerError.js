38cf9c05e88eb9268cfc33af09d4f0168201fc6e
Douglas Blank <douglasblank1@gmail.com>
2021-11-13 14:46:47 -0300

    feat: added utils to handle errors on middleware

--- api/src/utils/handlerError.js
+++ api/src/utils/handlerError.js
@@ -1,5 +1,15 @@
-export async function handler(event) {
-  console.log(event);
+import { getBody, lambdaResp } from '@/utils/utils';
+
+export async function handler(_, context) {
+  const { prev } = context;
+
+  const body = getBody(prev);
+  const { statusCode } = prev;
+
+  return lambdaResp(
+    statusCode || 400,
+    body.name || body,
+  );
 }
 
 export default handler;
