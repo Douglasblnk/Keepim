23d09e09eadcb5b0061689ecaa494c8acd9d229a
Douglas Blank <douglas.blank@multiplier.com.br>
2022-05-12 15:51:32 -0300

    feat: added unocss icon preset

--- app/unocss.config.js
+++ app/unocss.config.js
@@ -3,6 +3,7 @@ import presetWind from '@unocss/preset-wind'
 import presetAttributify from '@unocss/preset-attributify'
 import transformerDirective from '@unocss/transformer-directives'
 import presetMini from '@unocss/preset-mini'
+import presetIcons from '@unocss/preset-icons'
 import presetCore from './src/unocss/index'
 
 export default defineConfig({
@@ -11,6 +12,12 @@ export default defineConfig({
     presetWind(),
     presetCore(),
     presetAttributify(),
+    presetIcons({
+      extraProperties: {
+        display: 'inline-block',
+      },
+      warn: true,
+    }),
   ],
   transformers: [
     transformerDirective(),
