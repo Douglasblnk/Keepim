6dca614611fb4e5b933639c161755e45d1448844
Douglas Blank <douglasblank1@gmail.com>
2023-07-19 21:49:42 -0300

    feat(back): implement get lambda body utils functions

--- api/src/functions/auth/sign-in/handler.ts
+++ api/src/functions/auth/sign-in/handler.ts
@@ -1,20 +1,12 @@
-import { lambdaErrorResponse, lambdaOKResponse, middyfy } from '@utils/lambda'
+import { getLambdaBody, lambdaErrorResponse, lambdaOKResponse, middyfy } from '@utils/lambda'
 import type { CustomAPIGatewayProxyEvent } from '@type/api-gateway'
-import { errMissingParams } from '@exceptions/auth-exceptions'
 import { signIn } from '@service/auth'
 import { serialize } from 'cookie'
 import type { SignInSchemaBody } from './schema'
 
-const getBody = (body: SignInSchemaBody) => {
-  if (!body.username || !body.password)
-    throw errMissingParams()
-
-  return body
-}
-
 const handler = async (event: CustomAPIGatewayProxyEvent<SignInSchemaBody, any>) => {
   try {
-    const signInSchema = getBody(event.body)
+    const signInSchema = getLambdaBody(event.body, ['username', 'password'])
 
     const { accessToken, user } = await signIn(signInSchema)
 
