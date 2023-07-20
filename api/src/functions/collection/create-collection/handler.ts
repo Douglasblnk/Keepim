8bb45d600276a5caa4000ef12313fa07af267cb9
Douglas Blank <douglasblank1@gmail.com>
2023-07-19 21:57:03 -0300

    feat(back): implement collection request handler

--- api/src/functions/collection/create-collection/handler.ts
+++ api/src/functions/collection/create-collection/handler.ts
@@ -1,15 +1,22 @@
-import { lambdaErrorResponse, middyfy } from '@utils/lambda'
+import { getLambdaBody, lambdaErrorResponse, lambdaOKResponse, middyfy } from '@utils/lambda'
+import { joinWithContextUsername } from '@utils/utils'
 import type { CustomAPIGatewayProxyEvent } from '@type/api-gateway'
-import type { CollectionSchemaParams } from './schema'
+import { createCollection } from '@service/collection'
+import type { MiddlewareContext } from '@type/lambda'
+import type { CollectionSchemaBody } from './schema'
 
-const handler = async (event: CustomAPIGatewayProxyEvent<any, CollectionSchemaParams>) => {
+const handler = async (event: CustomAPIGatewayProxyEvent<CollectionSchemaBody, any>, context: MiddlewareContext) => {
   try {
+    const body = getLambdaBody(event.body, ['collectionDate', 'collectionName'])
 
+    const response = await createCollection(joinWithContextUsername(body, context))
+
+    return lambdaOKResponse(response)
   }
 
   catch (error) {
     console.log('error create-collection :>> ', error)
-    return lambdaErrorResponse({ error })
+    return lambdaErrorResponse(error)
   }
 }
 
