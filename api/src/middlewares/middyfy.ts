5a34b7825af70367ff46c2e48688780f57de5175
Douglas Blank <douglasblank1@gmail.com>
2023-07-23 21:28:24 -0300

    feat(back): add typing to middyfy function

--- api/src/middlewares/middyfy.ts
+++ api/src/middlewares/middyfy.ts
@@ -1,8 +1,19 @@
 import cors from '@middy/http-cors'
 import middyJsonBodyParser from '@middy/http-json-body-parser'
 import middy from '@middy/core'
+import type { CustomAPIGatewayProxyEvent } from '@type/api-gateway'
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
-export const middyfy = (handler, custom) => {
+interface CustomMiddleware {
+  before: middy.MiddlewareFn<CustomAPIGatewayProxyEvent<any, any>, APIGatewayProxyResult>
+  onError: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult>
+}
+
+interface HandlerFn<T> {
+  (event: CustomAPIGatewayProxyEvent<any, T>): Promise<{ statusCode: unknown; headers: { 'Access-Control-Allow-Headers': string; 'Access-Control-Allow-Origin': string } & object; body: string }>
+}
+
+export const middyfy = <T>(handler: HandlerFn<T>, custom?: CustomMiddleware) => {
   const middlewareChain = middy(handler)
     .use(middyJsonBodyParser())
     .use(cors({
