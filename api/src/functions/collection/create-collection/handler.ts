ac95130e7810d7b9efaa1b7ef5f04da1310587ae
Douglas Blank <douglasblank1@gmail.com>
2023-07-23 21:30:21 -0300

    feat(back): improve collection endpoint

--- api/src/functions/collection/create-collection/handler.ts
+++ api/src/functions/collection/create-collection/handler.ts
@@ -1,16 +1,16 @@
+import authenticationMiddleware from '@middleware/authentication'
 import { getLambdaBody, lambdaErrorResponse, lambdaOKResponse } from '@utils/lambda'
-import { joinWithContextUsername } from '@utils/utils'
 import type { CustomAPIGatewayProxyEvent } from '@type/api-gateway'
-import { createCollection } from '@service/collection'
-import type { MiddlewareContext } from '@type/lambda'
 import { middyfy } from '@middleware/middyfy'
+import createCollection from '@service/collection/create-collection'
+import { getUsernameFromBody } from '@utils/utils'
 import type { CollectionSchemaBody } from './schema'
 
-const handler = async (event: CustomAPIGatewayProxyEvent<CollectionSchemaBody, any>, context: MiddlewareContext) => {
+const handler = async (event: CustomAPIGatewayProxyEvent<CollectionSchemaBody, any>) => {
   try {
-    const body = getLambdaBody(event.body, ['collectionDate', 'collectionName'])
+    const body = getLambdaBody(event.body, ['collectionDate', 'collectionName', 'username'])
 
-    const response = await createCollection(joinWithContextUsername(body, context))
+    const response = await createCollection(body, getUsernameFromBody(body))
 
     return lambdaOKResponse(response)
   }
@@ -21,4 +21,4 @@ const handler = async (event: CustomAPIGatewayProxyEvent<CollectionSchemaBody, a
   }
 }
 
-export const main = middyfy(handler)
+export const main = middyfy(handler, authenticationMiddleware())
