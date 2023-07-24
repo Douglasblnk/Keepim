3f32dbe974b4a0d2153f2e69f5fa663a7d71bda1
Douglas Blank <douglasblank1@gmail.com>
2023-07-23 21:17:54 -0300

    feat(back): add auth middleware to middy function

--- api/src/middlewares/middyfy.ts
+++ api/src/middlewares/middyfy.ts
@@ -2,11 +2,16 @@ import cors from '@middy/http-cors'
 import middyJsonBodyParser from '@middy/http-json-body-parser'
 import middy from '@middy/core'
 
-export const middyfy = (handler) => {
-  return middy(handler)
+export const middyfy = (handler, custom) => {
+  const middlewareChain = middy(handler)
     .use(middyJsonBodyParser())
     .use(cors({
       credentials: true,
       origin: 'https://keepim.douglasblnk.com',
     }))
+
+  if (custom)
+    middlewareChain.use(custom)
+
+  return middlewareChain
 }
