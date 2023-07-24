8ce5cc14b74dfae4ed3ba72a9449c195ae09079f
Douglas Blank <douglasblank1@gmail.com>
2023-07-23 21:17:04 -0300

    feat(back): improve auth middleware to work on middy

--- api/src/middlewares/authentication.ts
+++ api/src/middlewares/authentication.ts
@@ -1,15 +1,41 @@
+import type middy from '@middy/core'
 import type { CustomAPIGatewayProxyEvent } from '@type/api-gateway'
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { getAccessToken } from '@utils/auth'
 import { validateAccessToken } from '@service/auth'
 import { errAuthorizationFailed } from '@exceptions/auth-exceptions'
+import { handleAuthMiddlewareError } from '@utils/utils'
 
-export const main = async (event: CustomAPIGatewayProxyEvent<any, {}>) => {
-  const accessToken = getAccessToken(event)
+const authenticationMiddleware = () => {
+  const before: middy.MiddlewareFn<CustomAPIGatewayProxyEvent<any, any>, APIGatewayProxyResult> = async (request) => {
+    try {
+      const accessToken = getAccessToken(request.event)
 
-  const session = await validateAccessToken(accessToken)
+      const session = await validateAccessToken(accessToken)
 
-  if (!session)
-    throw errAuthorizationFailed()
+      if (!session)
+        throw errAuthorizationFailed()
 
-  return session.username
+      request.event.body = {
+        ...request.event.body,
+        username: session.username,
+      }
+    }
+
+    catch (error) {
+      console.log('error authenticationMiddleware :>> ', error)
+      throw errAuthorizationFailed()
+    }
+  }
+
+  const onError: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = async (request) => {
+    return handleAuthMiddlewareError(request.error)
+  }
+
+  return {
+    before,
+    onError,
+  }
 }
+
+export default authenticationMiddleware
