307467741ae524c6bef962743187421ce08e1852
Douglas Blank <douglasblank1@gmail.com>
2023-07-27 13:47:51 -0300

    feat(front): work in progress of configuration page

---
+++ app/src/pages/configuration.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const { back } = useRouter()
+</script>
+
+<template>
+  <div un-p-lg>
+    <div
+      un-relative
+      un-flex
+      un-items-center
+      un-justify-between
+    >
+      <QIcon
+        name="i-mdi-arrow-left"
+        size="md"
+        un-absolute
+        @click="back"
+      />
+
+      <h1
+        un-title
+        un-mx-auto
+      >
+        Configurações
+      </h1>
+    </div>
+
+    <KInfoUser un-mt-xl />
+  </div>
+</template>
+
+<route lang="yaml">
+path: /configuracoes
+meta:
+  title: Configurações
+  transition: slide-down
+</route>
