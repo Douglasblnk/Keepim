304b3a2b58b945b34e530b4021aa661b602c1140
Douglas Blank <douglasblank1@gmail.com>
2023-07-27 13:46:41 -0300

    feat(front): implement route transition

--- app/src/plugins/router.js
+++ app/src/plugins/router.js
@@ -2,12 +2,20 @@ import routes from 'virtual:generated-pages'
 import { createRouter, createWebHistory } from 'vue-router'
 import handleRoutes from '../middleware/handle-route-auth'
 
+const { setRouteTransition } = useRouteTransition()
+
 export const router = createRouter({
   history: createWebHistory(),
   routes,
 })
 
-router.beforeEach((to, prev, next) => handleRoutes(to, prev, next))
+router.beforeEach((to, prev, next) => {
+  const { transition } = to.meta
+
+  setRouteTransition(transition)
+
+  return handleRoutes(to, prev, next)
+})
 
 export function install(app) {
   app.use(router)
