c6849a8af760415da643d2d4a83c6a91d4f215a0
Douglas Blank <douglasblank1@gmail.com>
2023-07-27 13:47:28 -0300

    feat(front): implement new user info component

---
+++ app/src/components/miscellaneous/KInfoUser.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const { userStorage } = useLocalStorage()
+</script>
+
+<template>
+  <div
+    un-flex
+    un-items-center
+    un-gap-md
+  >
+    <KAvatar
+      size="60px"
+      color="primary"
+      :img="userStorage.avatar"
+      :label="userStorage.name"
+    />
+
+    <div
+      un-flex="~ col"
+      un-gap-xs
+    >
+      <span
+        un-text-18px
+        un-font-bold
+      >
+        {{ userStorage.name }}
+      </span>
+
+      <span un-text-gray>{{ userStorage.email }}</span>
+    </div>
+  </div>
+</template>
