a013e5c7fcf5cfdde1d5fd0f7fa701bce42e48ed
Douglas Blank <douglasblank1@gmail.com>
2023-07-28 01:56:34 -0300

    feat(front): implement configuration page

--- app/src/pages/configuration.vue
+++ app/src/pages/configuration.vue
@@ -1,31 +1,114 @@
 <script setup lang="ts">
-const { back } = useRouter()
+const { back, replace } = useRouter()
+const { deleteStorageState } = useLocalStorage()
+
+const confirmLogout = ref(false)
+const timeout: Ref<NodeJS.Timeout | null> = ref(null)
+
+function preLogout() {
+  confirmLogout.value = true
+
+  timeout.value = setTimeout(() => {
+    confirmLogout.value = false
+  }, 2000)
+}
+
+async function logout() {
+  clearTimeout(timeout.value!)
+
+  const response = await signOutRequest()
+
+  if (response) {
+    deleteStorageState('user-info')
+    replace('/login')
+  }
+}
 </script>
 
 <template>
-  <div un-p-lg>
+  <div
+    un-p-lg
+    un-h-screen
+  >
     <div
-      un-relative
-      un-flex
-      un-items-center
-      un-justify-between
+      un-sm-w-col-8
+      un-md-w-col-5
+      un-mx-auto
+      un-h-full
+      un-flex="~ col"
     >
-      <QIcon
-        name="i-mdi-arrow-left"
-        size="md"
-        un-absolute
-        @click="back"
+      <div
+        un-relative
+        un-flex
+        un-items-center
+        un-justify-between
+      >
+        <QIcon
+          name="i-mdi-arrow-left"
+          size="md"
+          un-absolute
+          un-cursor-pointer
+          @click="back"
+        />
+
+        <h1
+          un-title
+          un-mx-auto
+        >
+          Configurações
+        </h1>
+      </div>
+
+      <KInfoUser un-mt-xl />
+
+      <ConfigButton
+        title="Editar perfil"
+        description="Altere suas informações de perfil"
+        icon="i-mdi-account-outline"
+        color="bg-#6A5590"
+        un-mt-lg
       />
 
-      <h1
-        un-title
-        un-mx-auto
-      >
-        Configurações
-      </h1>
-    </div>
+      <ConfigButton
+        title="Configurações"
+        description="Configure o tema da aplicação"
+        icon="i-mdi-cog-outline"
+        color="bg-#6D9ABB"
+        un-mt-sm
+      />
 
-    <KInfoUser un-mt-xl />
+      <div
+        un-flex="~ col-reverse"
+        un-w-full
+        un-flex-grow
+      >
+        <Transition
+          name="fade-100"
+          mode="out-in"
+        >
+          <ConfigButton
+            v-if="!confirmLogout"
+            title="Sair do sistema"
+            icon="i-mdi-logout"
+            color="bg-negative"
+            un-mt-lg
+            un-w-full
+            @click="preLogout"
+          />
+
+          <ConfigButton
+            v-else
+            title="Confirmar saída?"
+            icon="i-mdi-check"
+            color="bg-positive"
+            un-mt-lg
+            un-w-full
+            show-progress-bar
+            @click="logout"
+          />
+        </Transition>
+      </div>
+    </div>
   </div>
 </template>
 
