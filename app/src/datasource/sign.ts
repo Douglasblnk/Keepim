015d0f16e7e1955c1341029c581ee154fa7fec66
Douglas Blank <douglasblank1@gmail.com>
2023-07-28 01:56:16 -0300

    feat(front): implement signOut datasource function

--- app/src/datasource/sign.ts
+++ app/src/datasource/sign.ts
@@ -1,3 +1,5 @@
+import authMiddleware from '@/middleware/auth-middleware'
+
 interface SignInBody {
   username: string
   password: string
@@ -7,4 +9,12 @@ export async function signInRequest(body: SignInBody) {
   const { data } = await axios.post('/sign-in', body, { withCredentials: true })
 
   return data
+}
+
+export async function signOutRequest() {
+  return authMiddleware(async () => {
+    const { data } = await axios.post('/sign-out', {}, { withCredentials: true })
+
+    return data
+  })
 }
