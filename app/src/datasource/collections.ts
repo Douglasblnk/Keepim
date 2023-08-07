ed345bcfc2f570b0710b9e9167d322ec216978d5
Douglas Blank <douglasblank1@gmail.com>
2023-08-07 20:09:44 -0300

    feat(front): implement collections listing components and query

--- app/src/datasource/collections.ts
+++ app/src/datasource/collections.ts
@@ -1,9 +1,8 @@
+import type { CollectionBody, CollectionsParams, CollectionsResponse } from '@type/collection'
 import authMiddleware from '@middleware/auth-middleware'
-
-interface CollectionBody {
-  collectionName: string
-  collectionDate: string
-}
+import type { QueryFunctionContext } from '@tanstack/vue-query'
+import type { WritableComputedRef } from 'vue'
+import type { EvaluatedKeyPagination } from './../../../api/src/models/pagination'
 
 export async function createCollectionRequest(body: CollectionBody) {
   return authMiddleware(async () => {
@@ -16,6 +15,17 @@ export async function createCollectionRequest(body: CollectionBody) {
 export async function getCollectionsCountRequest() {
   return authMiddleware(async () => {
     const { data } = await axios.get('/collections-count', { withCredentials: true })
+
+    return data
+  })
+}
+
+export async function getCollectionsRequest({ queryKey, pageParam }: QueryFunctionContext<(string | WritableComputedRef<CollectionsParams>)[], any>) {
+  const params = queryKey[1] as unknown as CollectionsParams
+  const startKey = pageParam?.id
+
+  return authMiddleware<EvaluatedKeyPagination<CollectionsResponse[]>>(async () => {
+    const { data } = await axios.get('/collections', { withCredentials: true, params: { ...params, startKey } })
 
     return data
   })
