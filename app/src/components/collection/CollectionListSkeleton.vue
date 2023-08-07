475898cbb482bdc7b484ef003f3db19331394ac3
Douglas Blank <douglasblank1@gmail.com>
2023-08-07 20:08:33 -0300

    feat(front): implement collections list skeleton

---
+++ app/src/components/collection/CollectionListSkeleton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div
+      v-for="item in [5, 3, 2]"
+      :key="item"
+      un-space-y-md
+      un-mt-lg
+    >
+      <QSkeleton
+        un-w-110px
+        un-rounded-full
+      />
+
+      <div
+        un-flex
+        un-flex-wrap
+      >
+        <QSkeleton
+          v-for="(skeleton) in item"
+          :key="skeleton"
+          un-w-90px
+          un-h-90px
+          un-mr-sm
+          un-mb-sm
+          un-rounded-2xl
+        />
+      </div>
+    </div>
+  </div>
+</template>
