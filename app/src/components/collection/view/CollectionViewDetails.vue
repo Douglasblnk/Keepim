8c02c565e9882532cfba455098697a10161535a3
Douglas Blank <douglasblank1@gmail.com>
2023-08-22 20:38:40 -0300

    feat(front): implement collection view details components

---
+++ app/src/components/collection/view/CollectionViewDetails.vue
@@ -0,0 +1,130 @@
+<script setup lang="ts">
+import type { CollectionResponse } from '@/types'
+
+const collection = inject<Ref<CollectionResponse | undefined>>('collection')!
+
+const equipmentIcons: Record<string, string> = {
+  camera: 'i-mdi-camera-outline',
+  lens: 'i-mdi-camera-switch-outline',
+  flash: 'i-mdi-flash-outline',
+  tripod: 'i-custom-tripod',
+  memoryCard: 'i-mdi-sd',
+  filter: 'i-mdi-filter-outline',
+}
+
+const missingDetails = computed(() => {
+  return !collection.value?.participants?.length
+    && !collection.value?.equipments?.length
+    && !collection.value?.place
+})
+</script>
+
+<template>
+  <div
+    un-flex="~ col"
+    un-gap-md
+    un-justify-center
+  >
+    <span un-text="lg center">
+      {{ collection?.collectionName }}
+    </span>
+
+    <span un-text="center gray-text">
+      {{ collection?.description }}
+    </span>
+  </div>
+
+  <QSeparator
+    v-if="!missingDetails"
+    spaced="xl"
+    un-w-col-2
+    un-mx-auto
+    un-text-gray-text
+  />
+
+  <div
+    v-if="!missingDetails"
+    un-space-y-lg
+    un-mb-60px
+  >
+    <div
+      v-if="collection?.participants?.length"
+      un-space-y-md
+    >
+      <span un-font-bold>Participantes:</span>
+
+      <div
+        v-for="(participant, index) in collection?.participants"
+        :key="`participant-${index}`"
+        un-gap-sm
+      >
+        <KAvatar
+          :label="participant"
+          un-bg-primary
+          un-mr-sm
+        />
+
+        <span>{{ participant }}</span>
+      </div>
+    </div>
+
+    <div
+      v-if="collection?.equipments?.length"
+      un-space-y-md
+    >
+      <span un-font-bold>Equipamentos:</span>
+
+      <div
+        v-for="(equipment, index) in collection?.equipments"
+        :key="`equipment-${index}`"
+        un-gap-sm
+        un-flex
+        un-items-center
+      >
+        <QIcon
+          :name="equipmentIcons[equipment.type]"
+          size="sm"
+        />
+
+        <span>{{ equipment.name }}</span>
+      </div>
+    </div>
+
+    <div
+      v-if="collection?.place"
+      un-space-y-md
+    >
+      <span un-font-bold>Local:</span>
+
+      <div
+        un-flex
+        un-items-center
+        un-gap-sm
+      >
+        <QIcon
+          name="i-mdi-map-marker-outline"
+          size="sm"
+        />
+
+        <span>{{ collection?.place }}</span>
+      </div>
+    </div>
+  </div>
+
+  <div
+    v-else
+    un-mt-6xl
+    un-flex="~ col"
+    un-items-center
+    un-gap-md
+    un-text-gray-text
+  >
+    <span>Não há detalhes para esta coleção</span>
+
+    <QBtn
+      label="Adicionar detalhes"
+      color="primary"
+      icon="i-mdi-plus"
+    />
+  </div>
+</template>
