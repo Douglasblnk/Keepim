3bb6b6e346d3505a142418639cb31d6734175388
Douglas Blank <douglasblank1@gmail.com>
2023-08-22 20:39:04 -0300

    feat(front): implement collection view wrapper component

---
+++ app/src/components/collection/view/CollectionView.vue
@@ -0,0 +1,144 @@
+<script setup lang="ts">
+import { useGesture } from '@vueuse/gesture'
+import type { CollectionResponse } from '@/types'
+
+defineProps<{ isLoading: boolean }>()
+
+const collection = inject<Ref<CollectionResponse | undefined>>('collection')
+const isMobile = inject<ComputedRef<boolean>>('isMobile')
+
+const container = ref<HTMLDivElement | null>(null)
+const containerMarginTop = ref(60)
+const tab = ref('photos')
+
+if (isMobile?.value) {
+  useGesture({
+    onDrag: ({ values: [ _, y ] }) => {
+      const newValue = y - 190
+
+      if (newValue >= -80 && newValue <= 60)
+        containerMarginTop.value = newValue
+    },
+  }, { domTarget: container })
+}
+</script>
+
+<template>
+  <div
+    un-bg-dark-secondary
+    un-px-lg
+    un-border="rounded-t-36px"
+    un-z-10
+    un-grow
+    un-flex="~ col"
+    :un-transition="!isMobile && 'all 0.3s ease'"
+    :style="{ marginTop: `${containerMarginTop}px` }"
+  >
+    <div
+      v-if="isMobile"
+      ref="container"
+      un-flex
+      un-justify-center
+      un-cursor-grab
+    >
+      <div
+        un-rounded-full
+        un-bg-dark-primary
+        un-w-14
+        un-h-1
+        un-mt-md
+        un-mb-md
+      />
+    </div>
+
+    <div
+      v-else
+      un-flex
+      un-justify-center
+    >
+      <div
+        un-w-10
+        un-h-10
+        un-flex
+        un-items-center
+        un-justify-center
+        un-rounded-full
+        un-bg-dark-secondary
+        un-cursor-pointer
+        un-mt="-20px"
+        @click="containerMarginTop = containerMarginTop === 60 ? -75 : 60"
+      >
+        <QIcon
+          :name="containerMarginTop === 60 ? 'i-mdi-arrow-expand-up' : 'i-mdi-arrow-expand-down'"
+          size="sm"
+          un-text-gray-text
+        />
+      </div>
+    </div>
+
+    <div un-flex="~ col grow">
+      <QTabs
+        v-model="tab"
+        inline-label
+        align="justify"
+        no-caps
+        indicator-color="transparent"
+        active-color="primary"
+        un-rounded-xl
+        un-mb-md
+      >
+        <QTab
+          name="photos"
+          icon="i-mdi-image-outline"
+          label="Fotos"
+        />
+        <QTab
+          name="info"
+          icon="i-mdi-information-outline"
+          label="Detalhes"
+        />
+      </QTabs>
+
+      <QTabPanels
+        v-model="tab"
+        class="collection-tab-panel"
+        animated
+        un-bg-transparent
+        un-flex
+        un-grow
+      >
+        <QTabPanel
+          name="photos"
+          un-flex
+          un-p-none
+        >
+          <div un-flex="~ grow">
+            <QScrollArea un-grow>
+              <CollectionViewPhotosSkeleton v-if="isLoading" />
+
+              <CollectionViewPhotos />
+            </QScrollArea>
+          </div>
+        </QTabPanel>
+
+        <QTabPanel
+          name="info"
+          un-p-none
+          un-flex
+        >
+          <QScrollArea un-grow>
+            <CollectionViewDetailsSkeleton v-if="isLoading" />
+
+            <CollectionViewDetails v-else />
+          </QScrollArea>
+        </QTabPanel>
+      </QTabPanels>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.collection-tab-panel :deep(> div)
+  height: auto !important
+  flex-grow: 1
+</style>
--- app/src/pages/collections/[id].vue
+++ app/src/pages/collections/[id].vue
@@ -1,17 +1,55 @@
 <script setup lang="ts">
+import { capitalizeFirstLetter, dayjs } from '@utils'
+
 const props = defineProps<{
   id: string
 }>()
 
-useHead({
-  title: computed(() => `Keepim - ${props.id}`),
+useHead({ title: computed(() => `Keepim - ${props.id}`) })
+
+const { data, isLoading } = useQuery({
+  queryKey: [ 'collection', props.id ],
+  queryFn: () => getCollectionRequest(props.id),
+})
+
+const collectionDate = computed(() => {
+  const dayjsDate = dayjs(data.value?.collectionDate)
+
+  return `
+    ${dayjsDate.format('DD')}
+    de
+    ${capitalizeFirstLetter(dayjsDate.format('MMMM [de] YYYY'))}
+  `.trim()
+})
+
+provide('collection', data)
+
+onMounted(() => {
+  document.body.style.overflow = 'hidden'
+})
+
+onUnmounted(() => {
+  document.body.style.overflow = 'auto'
 })
 </script>
 
 <template>
-  <KLayoutPage :default-header="id">
-    poxa
-  </KLayoutPage>
+  <div
+    un-flex="~ col grow"
+    un-w="sm:col-8 lg:col-6 col-12"
+    un-relative
+  >
+    <CollectionViewHeader
+      :collection-name="data?.collectionName"
+      :collection-date="collectionDate"
+      :is-loading="isLoading"
+    />
+
+    <CollectionView
+      :collection="data"
+      :is-loading="isLoading"
+    />
+  </div>
 </template>
 
 <route lang="yaml">
