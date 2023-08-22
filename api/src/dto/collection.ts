0901532192f76f2ede9a4535754da57e3d83bb5c
Douglas Blank <douglasblank1@gmail.com>
2023-08-22 20:25:04 -0300

    refactor(back): improve collection DTO

--- api/src/dto/collection.ts
+++ api/src/dto/collection.ts
@@ -1,21 +1,37 @@
 import type { CollectionModel } from '@model/collection'
 import type { EvaluatedKeyPagination } from '@model/pagination'
 
-export interface CollectionDTO {
+export interface CompactCollectionDTO {
+  id: string
   collectionName: string
   collectionDate: string
 }
 
-export const collectionDTO = (collection: CollectionModel): CollectionDTO => {
+export type CollectionDTO = Omit<CollectionModel, 'createdAt' | 'updatedAt' | 'deleted'>
+
+export const compactCollectionDTO = (collection: CollectionModel): CompactCollectionDTO => {
   return {
+    id: collection?.id,
     collectionName: collection?.collectionName,
     collectionDate: collection?.collectionDate,
   }
 }
 
-export const collectionsListDTO = (collections: EvaluatedKeyPagination<CollectionModel[]>): EvaluatedKeyPagination<CollectionDTO[]> => {
+export const collectionDTO = (collection: CollectionModel): CollectionDTO => {
+  return {
+    id: collection?.id,
+    collectionName: collection?.collectionName,
+    collectionDate: collection?.collectionDate,
+    description: collection?.description,
+    equipments: collection?.equipments,
+    participants: collection?.participants,
+    place: collection?.place,
+  }
+}
+
+export const collectionsListDTO = (collections: EvaluatedKeyPagination<CollectionModel[]>): EvaluatedKeyPagination<CompactCollectionDTO[]> => {
   return {
     ...collections,
-    data: collections.data.map(collectionDTO),
+    data: collections.data.map(compactCollectionDTO),
   }
 }
