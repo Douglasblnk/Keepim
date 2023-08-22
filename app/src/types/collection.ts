8ccc21fdf5b356d63ca22a92c0122aa38cbdf546
Douglas Blank <douglasblank1@gmail.com>
2023-08-22 20:30:35 -0300

    feat(front): implement get collection request and types

--- app/src/types/collection.ts
+++ app/src/types/collection.ts
@@ -3,13 +3,30 @@ export interface CollectionBody {
   collectionDate: string
 }
 
-export interface CollectionsResponse {
-  collectionName: string
-  collectionDate: string
-}
-
 export interface CollectionsParams {
   search: string
   orderBy: 'ASC' | 'DESC'
   sortBy: 'collectionName' | 'collectionDate'
+}
+
+export interface CollectionsResponse {
+  id: string
+  collectionName: string
+  collectionDate: string
+}
+
+interface Equipment {
+  type: string
+  name: string
+}
+
+export interface CollectionResponse {
+  id: string
+  username?: string
+  collectionDate: string
+  collectionName: string
+  description?: string
+  equipments?: Equipment[]
+  participants?: string[]
+  place?: string
 }
