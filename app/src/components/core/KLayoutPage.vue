65788b9554f8ecde73bac5f69859e8890ba7a26b
Douglas Blank <douglasblank1@gmail.com>
2023-08-22 20:35:22 -0300

    feat(front): implement SimpleHeader component for KLayoutPage header

--- app/src/components/core/KLayoutPage.vue
+++ app/src/components/core/KLayoutPage.vue
@@ -3,56 +3,28 @@ defineOptions({
   inheritAttrs: false,
 })
 
-defineProps<{
-  defaultHeader?: string
-  withBlur?: boolean
-}>()
+defineProps<{ withBlur?: boolean }>()
 
-const { back } = useRouter()
 const attrs = useAttrs()
 </script>
 
 <template>
   <div un-flex="~ col grow">
     <div
-      v-if="defaultHeader || $slots.header"
+      v-if="$slots.header"
       un-pos-sticky
       un-top-0
       un-z-10
-      un-pt-lg
       :style="withBlur ? { background: 'rgba(32, 36, 39, 0.70)' } : {}"
       :class="withBlur ? 'backdrop-blur-md' : 'bg-background'"
     >
       <div
         un-w="sm:col-8 lg:col-6 col-12"
         un-p-x-lg
+        un-pt-lg
         v-bind="attrs"
       >
-        <slot name="header">
-          <div
-            v-if="defaultHeader"
-            un-relative
-            un-flex
-            un-items-center
-            un-justify-between
-            un-pb-md
-          >
-            <QIcon
-              name="i-mdi-arrow-left"
-              size="md"
-              un-absolute
-              un-cursor-pointer
-              @click="back"
-            />
-
-            <h1
-              un-title
-              un-mx-auto
-            >
-              {{ defaultHeader }}
-            </h1>
-          </div>
-        </slot>
+        <slot name="header" />
       </div>
     </div>
 
---
+++ app/src/components/core/headers/SimpleHeader.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  isLoading?: boolean
+}>()
+
+const { back } = useRouter()
+</script>
+
+<template>
+  <div
+    un-relative
+    un-flex
+    un-items-center
+    un-justify-between
+    un-pb-md
+  >
+    <QIcon
+      name="i-mdi-arrow-left"
+      size="md"
+      un-absolute
+      un-cursor-pointer
+      @click="back"
+    />
+
+    <h1
+      v-if="!isLoading"
+      un-title
+      un-mx-auto
+      un-max-w="70%"
+      un-truncate
+    >
+      {{ title }}
+    </h1>
+
+    <QSkeleton
+      v-else
+      un-w="40%"
+      un-h="24px"
+      un-rounded-full
+      un-mx-auto
+      un-my-md
+    />
+  </div>
+</template>
