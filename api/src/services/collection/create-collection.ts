970b819f9a8fd90173d8809e97f6132416e78cb8
Douglas Blank <douglasblank1@gmail.com>
2023-08-22 20:26:17 -0300

    refactor(back): apply compactCollectionDTO on collection creation return

--- api/src/services/collection/create-collection.ts
+++ api/src/services/collection/create-collection.ts
@@ -1,12 +1,12 @@
 import { createCollectionModel } from '@model/collection'
 import type { CollectionSchemaBody } from '@functions/collection/create-collection/schema'
 import { putCollection } from '@repository/collection'
-import { collectionDTO } from '@dto/collection'
+import { compactCollectionDTO } from '@dto/collection'
 
 export default async (body: CollectionSchemaBody) => {
   const collectionModel = createCollectionModel(body)
 
   await putCollection(collectionModel)
 
-  return collectionDTO(collectionModel)
+  return compactCollectionDTO(collectionModel)
 }
