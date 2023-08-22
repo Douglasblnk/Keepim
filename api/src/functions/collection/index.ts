4320538b9ee672338aaed49d6618b0b623daece5
Douglas Blank <douglasblank1@gmail.com>
2023-08-22 20:26:54 -0300

    feat(back): implement get collection endpoint

--- api/src/functions/collection/index.ts
+++ api/src/functions/collection/index.ts
@@ -1,9 +1,11 @@
 import CreateCollection from './create-collection'
 import GetCollectionsCount from './get-collections-count'
 import GetCollections from './get-collections'
+import GetCollection from './get-collection'
 
 export default {
   CreateCollection,
   GetCollectionsCount,
   GetCollections,
+  GetCollection,
 }
--- api/src/services/collection/index.ts
+++ api/src/services/collection/index.ts
@@ -1,7 +1,11 @@
 import createCollection from './create-collection'
 import getCollectionsCount from './get-collections-count'
+import getCollections from './get-collections'
+import getCollection from './get-collection'
 
 export {
   createCollection,
   getCollectionsCount,
+  getCollections,
+  getCollection,
 }
