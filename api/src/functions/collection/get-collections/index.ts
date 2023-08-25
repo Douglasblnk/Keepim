f9264786b8f5b7f5d04ded97810b2ab68517392e
Douglas Blank <douglasblank1@gmail.com>
2023-08-24 23:51:17 -0300

    fix(back): add iam role statement to get collections endpoint

--- api/src/functions/collection/get-collections/index.ts
+++ api/src/functions/collection/get-collections/index.ts
@@ -20,6 +20,16 @@ export default {
     },
     {
       Effect: 'Allow',
+      Action: ['dynamodb:Query'],
+      Resource: 'arn:aws:dynamodb:sa-east-1:531760387770:table/Keepim.Collection/index/collection-date-index',
+    },
+    {
+      Effect: 'Allow',
+      Action: ['dynamodb:Query'],
+      Resource: 'arn:aws:dynamodb:sa-east-1:531760387770:table/Keepim.Collection/index/collection-name-index',
+    },
+    {
+      Effect: 'Allow',
       Action: ['dynamodb:GetItem'],
       Resource: 'arn:aws:dynamodb:sa-east-1:531760387770:table/Keepim.Session',
     },
