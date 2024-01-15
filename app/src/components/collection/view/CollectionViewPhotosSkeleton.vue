73a6e23ef832d3524d2a0178d1ef338e5f0ceede
Douglas Blank <douglasblank1@gmail.com>
2024-01-15 02:37:24 -0300

    feat: implement dynamic size to collection view photos skeleton

--- app/src/components/collection/view/CollectionViewPhotosSkeleton.vue
+++ app/src/components/collection/view/CollectionViewPhotosSkeleton.vue
@@ -1,15 +1,25 @@
+<script setup lang="ts">
+const ImgListingSkeletonRef = ref<HTMLDivElement | null>()
+
+const { folderSize } = useListSize(ImgListingSkeletonRef, 16)
+</script>
+
 <template>
   <div
+    ref="ImgListingSkeletonRef"
     un-w-full
     un-flex
     un-flex-wrap
-    un-gap-sm
+    un-gap-md
   >
     <QSkeleton
       v-for="index in 18"
       :key="index"
-      un-w-125px
-      un-h-125px
+      un-max-w-200px
+      un-max-h-200px
+      un-min-w-85px
+      un-min-h-85px
+      :style="folderSize"
       un-rounded-3xl
     />
   </div>
