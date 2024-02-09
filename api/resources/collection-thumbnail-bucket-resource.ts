723f363960e114d6bea1d024e2a7581fb95306fc
Douglas Blank <douglasblank1@gmail.com>
2024-02-09 10:39:32 -0300

    feat: implement buckets resource

---
+++ api/resources/collection-thumbnail-bucket-resource.ts
@@ -0,0 +1,26 @@
+export default {
+  CollectionThumbnailBucket: {
+    Type: 'AWS::S3::Bucket',
+    Properties: {
+      BucketName: 'douglasblnk-keepim-dev-thumbnail-storage',
+      CorsConfiguration: {
+        CorsRules: [
+          {
+            AllowedHeaders: [
+              '*',
+            ],
+            AllowedMethods: [
+              'PUT',
+              'GET',
+              'DELETE',
+            ],
+            AllowedOrigins: [
+              '*',
+            ],
+            MaxAgeSeconds: 3000,
+          },
+        ],
+      },
+    },
+  },
+}
