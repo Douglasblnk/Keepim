be3b1f81db523eb6bab3134a24d5797262f7c0cd
Douglas Blank <douglasblank1@gmail.com>
2024-02-09 10:40:47 -0300

    feat: implement resize endpoint s3 trigger

--- api/src/services/photos/index.ts
+++ api/src/services/photos/index.ts
@@ -1,7 +1,11 @@
 import getSignedUploadUrl from './get-signed-upload-url'
 import getSignedUrls from './get-signed-urls'
+import deletePhotos from './delete-photos'
+import createPhotoThumbnails from './create-photo-thumbnails'
 
 export {
+  createPhotoThumbnails,
   getSignedUploadUrl,
   getSignedUrls,
+  deletePhotos,
 }
