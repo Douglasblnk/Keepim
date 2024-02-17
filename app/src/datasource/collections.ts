1bf57f648e1cb811d3ac8c07bae9398668928ca2
Douglas Blank <douglasblank1@gmail.com>
2024-02-17 13:38:21 -0300

    feat: implement favorite collection datasource

--- app/src/datasource/collections.ts
+++ app/src/datasource/collections.ts
@@ -19,6 +19,14 @@ export async function updateCollectionRequest({ body, collectionId }: { body: Pa
   })
 }
 
+export async function makeCollectionFavoriteRequest(collectionId: string) {
+  return authMiddleware<boolean>(async () => {
+    const { data } = await axios.patch(`/favorite-collection/${collectionId}`, {}, { withCredentials: true })
+
+    return data
+  })
+}
+
 export async function deleteCollectionRequest(collectionId: string) {
   return authMiddleware(async () => {
     const { data } = await axios.delete(`/collection/${collectionId}`, { withCredentials: true })
