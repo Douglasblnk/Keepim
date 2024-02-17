e16affb6d7454cf472325153d55a73147744f8f4
Douglas Blank <douglasblank1@gmail.com>
2024-02-17 13:37:48 -0300

    feat: implement favorite collection menu action

--- app/src/components/collection/view/CollectionViewHeaderOptions.vue
+++ app/src/components/collection/view/CollectionViewHeaderOptions.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { getErrorMsg, notify } from '@/utils'
+
 defineProps<{ contextMenu?: boolean }>()
 
 const emit = defineEmits<{
@@ -6,14 +8,49 @@ const emit = defineEmits<{
   openAddImageDialog: []
   removePhotoAction: []
   addCoverAction: []
+  makeFavorite: []
   deleteCollectionAction: []
 }>()
+
+const queryClient = useQueryClient()
 
 const store = useCollectionStore()
 
 const { collection } = storeToRefs(store)
 
 const menuActions = ref()
+
+const {
+  mutate,
+  isPending,
+} = useMutation({
+  mutationFn: makeCollectionFavoriteRequest,
+  onError: (error) => {
+    notify({
+      type: 'negative',
+      message: getErrorMsg(error) as string,
+    })
+  },
+  onSuccess: async (data) => {
+    if (data !== undefined) {
+      await notify({
+        type: 'positive',
+        message: 'Coleção favoritada',
+        timeout: 800,
+      })
+
+      menuActions.value.hide()
+
+      queryClient.invalidateQueries({ queryKey: [ 'collection' ] })
+      queryClient.invalidateQueries({ queryKey: [ 'collections-list' ] })
+    }
+  },
+})
+
+function makeFavorite() {
+  if (collection?.value?.id)
+    mutate(collection.value.id)
+}
 </script>
 
 <template>
@@ -22,13 +59,17 @@ const menuActions = ref()
     transition-show="jump-down"
     transition-hide="jump-up"
     :context-menu="contextMenu"
-    auto-close
   >
     <QList un-min-w-260px>
       <QItem
         v-if="!contextMenu"
-        un-pr-none
-        un-pt-none
+        un-p-y-none
+        un-md-p-r-none
+        un-sticky
+        un-top-0
+        un-mb-sm
+        un-z-1
+        un-bg-dark-primary
       >
         <QItemSection un-title>
           Ações
@@ -49,7 +90,7 @@ const menuActions = ref()
       <QItem
         v-if="!contextMenu"
         clickable
-        @click="emit('editCollection')"
+        @click="emit('editCollection'); menuActions.hide()"
       >
         <QItemSection
           avatar
@@ -64,26 +105,9 @@ const menuActions = ref()
       </QItem>
 
       <QItem
-        v-if="collection?.photos?.imgs?.length"
-        clickable
-        @click="emit('addCoverAction')"
-      >
-        <QItemSection
-          avatar
-          un-min-w-auto
-        >
-          <QIcon name="i-mdi-image-frame" />
-        </QItemSection>
-
-        <QItemSection un-text-gray>
-          Definir {{ contextMenu ? 'como' : '' }} capa
-        </QItemSection>
-      </QItem>
-
-      <QItem
         v-if="!contextMenu"
         clickable
-        @click="emit('openAddImageDialog')"
+        @click="emit('openAddImageDialog'); menuActions.hide()"
       >
         <QItemSection
           avatar
@@ -98,9 +122,57 @@ const menuActions = ref()
       </QItem>
 
       <QItem
+        v-if="!contextMenu"
+        clickable
+        @click="makeFavorite"
+      >
+        <QItemSection
+          avatar
+          un-min-w-auto
+        >
+          <QIcon
+            v-if="!isPending"
+            :name="collection?.favorite ? 'i-mdi-heart' : 'i-mdi-heart-outline'"
+          />
+
+          <QIcon
+            v-else
+            name="i-mdi-loading"
+            class="animate-spin"
+          />
+        </QItemSection>
+
+        <QItemSection un-text-gray>
+          {{ collection?.favorite ? 'Desfavoritar coleção' : 'Favoritar coleção' }}
+        </QItemSection>
+      </QItem>
+
+      <QSeparator
+        v-if="collection?.photos?.imgs?.length && !contextMenu"
+        spaced="md"
+      />
+
+      <QItem
         v-if="collection?.photos?.imgs?.length"
         clickable
-        @click="emit('removePhotoAction')"
+        @click="emit('addCoverAction'); menuActions.hide()"
+      >
+        <QItemSection
+          avatar
+          un-min-w-auto
+        >
+          <QIcon name="i-mdi-image-frame" />
+        </QItemSection>
+
+        <QItemSection un-text-gray>
+          Definir {{ contextMenu ? 'como' : '' }} capa
+        </QItemSection>
+      </QItem>
+
+      <QItem
+        v-if="collection?.photos?.imgs?.length"
+        clickable
+        @click="emit('removePhotoAction'); menuActions.hide()"
       >
         <QItemSection
           avatar
@@ -114,11 +186,16 @@ const menuActions = ref()
         </QItemSection>
       </QItem>
 
+      <QSeparator
+        v-if="!contextMenu"
+        spaced="md"
+      />
+
       <QItem
         v-if="!contextMenu"
         clickable
         un-color-negative
-        @click="emit('deleteCollectionAction')"
+        @click="emit('deleteCollectionAction'); menuActions.hide()"
       >
         <QItemSection
           avatar
