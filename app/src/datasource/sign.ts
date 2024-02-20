9deafc2fddee344a7d720cf021d3265acf795501
Douglas Blank <douglasblank1@gmail.com>
2024-02-19 22:35:50 -0300

    feat: add sign in response typing

--- app/src/datasource/sign.ts
+++ app/src/datasource/sign.ts
@@ -5,8 +5,23 @@ interface SignInBody {
   password: string
 }
 
+interface SignInResponse {
+  user: {
+    email: string
+    name: string
+    username: string
+  }
+  userConfig: {
+    collectionConfig: {
+      fixedCollection: number
+    }
+    id: string
+    username: string
+  }
+}
+
 export async function signInRequest(body: SignInBody) {
-  const { data } = await axios.post('/sign-in', body, { withCredentials: true })
+  const { data } = await axios.post<SignInResponse>('/sign-in', body, { withCredentials: true })
 
   return data
 }
